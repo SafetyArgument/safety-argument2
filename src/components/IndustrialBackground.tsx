b4acd39ca6bf7ffd5cb03f4e27b6366147904884
SafetyArgument <andriy@safetyargument.com.au>
2026-04-13 20:08:58 +0930

    Update IndustrialBackground.tsx

--- src/components/IndustrialBackground.tsx
+++ src/components/IndustrialBackground.tsx
@@ -4,7 +4,7 @@ export const IndustrialBackground: React.FC = () => {
   return (
     <div className="fixed inset-0 overflow-hidden pointer-events-none flex items-center justify-center">
       <img
-        src="/Images/backg.svg"
+        src="/safety-argument2/Images/backg.svg"
         alt="Industrial Background"
         className="w-full h-full object-cover rotate-90 dark:invert transition-all duration-300 opacity-[0.03]"
         referrerPolicy="no-referrer"
